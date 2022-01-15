2020c0b980d23024e8ce8a355120f5dddfcb4141
SachaAttiach <settiach@hotmail.co.uk>
2022-01-15 13:12:05 +0200

    channelListcontainer start

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth } from '.';
+
+import 'stream-chat-react/dist/css/index.css';
+import './App.css';
+
+const cookies = new Cookies();
+
+const apiKey = 'peztz2wv579x';
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey);
+
+
+
+
+const App = () => {
+
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                   
+                />
+                <ChannelContainer 
+                 
+                />
+            </Chat>
+        </div>
+    );
+}
+
+export default App;
